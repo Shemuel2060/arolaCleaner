de8034b4dfea8576ce04a6e008782b81445e0278
Shemuel2060 <2060newty@gmail.com>
2024-01-05 14:04:08 +0300

    data binding with a DTO

--- src/main/java/com/arola/as/controllers/ContactFormController.java
+++ src/main/java/com/arola/as/controllers/ContactFormController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.arola.as.api.Visitors;
+
 @Controller
 public class ContactFormController {
 	
@@ -16,12 +18,11 @@ public class ContactFormController {
 	// handler for processing form
 	
 	@RequestMapping("/processContactForm")
-	public String processContactForm(@RequestParam String name, @RequestParam String email,
-			@RequestParam String message, Model model) { /*args names the same as used in the form input tags */
+	public String processContactForm(Visitors visitor, Model model) { 
+		
 		//the model for transferring data from controller to view
-		model.addAttribute("visitorName", name);
-		model.addAttribute("visitorEmail", email);
-		model.addAttribute("visitorMessage", message);
+		model.addAttribute("visitor", visitor);
+		
 		// access the attribute names in the view page to get the inputs and display them
 		
 		return "processedContact-form";
